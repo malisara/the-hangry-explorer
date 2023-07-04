c0d7665dde93e64ae2496cf35332ab9bb4d9accb
Sara Mali <mali.sara7@gmail.com>
2023-07-04 15:53:38 +0200

    deploy gh pages II

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config https://vitest.dev/config
 export default defineConfig({
-  base: '/the-hangry-explorer/',
+  base: '/',
   plugins: [react(), tsconfigPaths()],
   build: {
     outDir: 'docs'
